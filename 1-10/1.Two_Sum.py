f2f8e0ce3bbcbf3502fc2787b7c2ae1818502c0a
Dod-o <estyledodo@qq.com>
2019-01-17 14:54:19 +0800

    add problem[1]

---
+++ 1-10/1.Two_Sum.py
@@ -0,0 +1,32 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = sorted(nums)
+        i = 0
+        j = len(n) - 1
+        while i < j:
+            sum = n[i] + n[j]
+            if sum > target:
+                j -= 1
+                while i < j and n[j] == n[j + 1]: j -= 1
+            elif sum < target:
+                i += 1
+                while i < j and n[i] == n[i - 1]: i += 1
+            elif sum == target:
+                if n[i] == n[j]:
+                    n1 = nums.index(n[i])
+                    del nums[n1]
+                    n2 = nums.index(n[j]) + 1
+                else:
+                    n1 = nums.index(n[i])
+                    n2 = nums.index(n[j])
+                return [n1, n2]
+
+
+
+s = Solution()
+print(s.twoSum([3, 3], 6))
