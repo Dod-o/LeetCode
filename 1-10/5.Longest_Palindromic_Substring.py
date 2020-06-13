ac8942307aa5f643129b8fbe7894d06bc249dcc4
Dod-o <tengchaolv@pku.edu.cn>
2020-06-13 23:55:31 +0800

    update problem 1

--- 1-10/5.Longest_Palindromic_Substring.py
+++ 1-10/5.Longest_Palindromic_Substring.py
@@ -1,54 +1,49 @@
-class Solution:
+class Solution(object):
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
-        ln = len(s)
+        if len(s) == 0: return ""
+        if len(s) == 1: return s
 
-        length = 0
-        index = 0
+        max_length = 0
+        max_sub_string = ""
 
-        maxLength = 0
-        maxIndex = 0
-        for i in range(ln - 1):
-            if s[i] == s[i + 1]:
-                point1 = i - 1
-                point2 = i + 2
-                length = point2 - point1 - 1
-                index = point1 + 1
-                while point1 >= 0 and point2 <= ln - 1:
-                    if s[point1] == s[point2]:
-                        point1 -= 1
-                        point2 += 1
-                    else:
-                        length = point2 - point1 - 1
-                        index = point1 + 1
-                        break
-                if point1 < 0 or point2 > ln - 1:
-                    length = point2 - point1 - 1
-                    index = point1 + 1
-                if length > maxLength:
-                    maxLength = length
-                    maxIndex = index
-        for i in range(maxLength // 2, ln):
-            point1 = i - 1
-            point2 = i + 1
-            length = point2 - point1 - 1
-            index = point1 + 1
-            while point1 >= 0 and point2 <= ln - 1:
-                if s[point1] == s[point2]:
-                    point1 -= 1
-                    point2 += 1
+        i = 0
+        while i < len(s) - 1:
+            if i % 1 == 0.5:
+                left, right = int(i - 0.5), int(i + 0.5)
+            else:
+                left, right = int(i), int(i)
+            cnt = 0
+            while left >= 0 and right <= len(s) - 1:
+                if s[left] == s[right]:
+                    left -= 1
+                    right += 1
+                    cnt += 1
                 else:
-                    length = point2 - point1 - 1
-                    index = point1 + 1
                     break
-            if point1 < 0 or point2 > ln - 1:
-                length = point2 - point1 - 1
-                index = point1 + 1
 
-            if length > maxLength:
-                maxLength = length
-                maxIndex = index
-        return s[maxIndex: maxIndex + maxLength]
+            if i % 1 == 0.5 and cnt * 2 > max_length:
+                max_length = cnt * 2
+                max_sub_string = s[left + 1: right]
+            if i % 1 == 0 and cnt * 2 - 1 > max_length:
+                max_length = cnt * 2 - 1
+                max_sub_string = s[left + 1: right]
+
+            if (len(s) - 1 - i) * 2 + 1 < max_length: return max_sub_string
+            i += 0.5
+
+        return max_sub_string
+
+
+
+
+
+
+if __name__ == '__main__':
+    a = ['a' for _ in range(1000)]
+    s = Solution()
+    result = s.longestPalindrome(a)
+    print(len(result))
